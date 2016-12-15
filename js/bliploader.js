5dc60f3c6aad0fd0e708859a9b63e427188bbe45
Fredrik Mäkilä <fredrik.makila@gmail.com>
2016-12-15 23:58:49 +0100

    Added some more animations

--- js/bliploader.js
+++ js/bliploader.js
@@ -5,6 +5,11 @@
   var originX;
   var originY;
   var ctx;
+  var numArcs = 24;
+
+  var counter = 1;
+  var ticksCount = 0;
+  var ticksPerCounter = 4;
 
   var config = require('./blipconfig').config();
 
@@ -49,45 +54,38 @@
     return createdCanvas;
   }
 
-  exports.start = function() {
-    drawBlipClock();
-    var runEverySecond = function() {
-      drawBlipClock();
-    };
-    window.setInterval(runEverySecond, 1000);
-
-    return canvas;
-  };
-
   function draw() {
+    updateCounter();
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
 
     var time = new Date();
 
     ctx.translate(originX, originY);
-    ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds());
+    ctx.rotate(
+      ((2*Math.PI)/60) * time.getSeconds() +
+      ((2*Math.PI)/60000) * time.getMilliseconds()
+    );
     ctx.translate(-originX, -originY);
 
     drawArcs();
 
     ctx.restore();
     window.requestAnimationFrame(draw);
-//    drawMinutes(minutes, canvas);
-  //  drawSeconds(seconds, canvas);
   }
 
   function drawArcs() {
     var radius = Math.min(canvas.width/2, canvas.height/2);
     var outerMargin = radius*0.1;
     var innerMargin = radius*0.2;
-    var arcDegree = Math.PI/24.0;
+    var arcDegree = Math.PI/numArcs;
 
     var outerRadius = radius - outerMargin;
     var innerRadius = radius - innerMargin;
 
-    for(var i = 1; i <= 24; i++) {
-      var active = i % 2;
+    for(var i = 1; i <= numArcs; i++) {
+      var active = i <= counter;
       drawPath(outerRadius, innerRadius, arcDegree, active, i);
     }
   };
@@ -106,5 +104,16 @@
     ctx.fillStyle = active ? config.colorOn : config.colorOff;
     ctx.fill();
   };
+
+  function updateCounter() {
+    ticksCount += 1;
+    if (ticksCount > ticksPerCounter) {
+      ticksCount = 0;
+      counter += 1;
+      if (counter > numArcs) {
+        counter = 1;
+      }
+    }
+  }
 
 })();
